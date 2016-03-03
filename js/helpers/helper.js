fc0e11fce36f644164d1b4094571e45babcb6d5a
shishir arora <shishirarora3@users.noreply.github.com>
2016-03-03 09:28:05 +0530

    Update helper.js

--- js/helpers/helper.js
+++ js/helpers/helper.js
@@ -1,48 +1,46 @@
 "use strict";
 var Helpers = {
-	findStrings:function(content, strings){
-		var len = strings.length;
-		var spaceGroups = `\\s*\\r*\\n*\\t*`;
-		var capturingGroup = `\"(.+)\"`
-		
-		var patternString = strings.reduce((res,str,i) => {
-			res += `${str}${spaceGroups}:\\s*\\"(.+)\\"`;
-			console.log(i);
-			if(i !== len-1){
-				res+=`\\,${spaceGroups}`;
-			}
-			return res;
-		},'');
-		//var pattern = "/name\s*\r*\n*\t*:\s*\"(.+)\"\,\s*\r*\n*\t*birthday\s*\r*\n*\t*:\s*\"(.+)\"/g";
-		return patternString;
-	},
-	dayMap:{
-		0:'sun',
-		1:'mon',
-		2:'tue',
-		3:'wed',
-		4:'thu',
-		5:'fri',
-		6:'sat'
-	},
-	findWidth: function(){
-			var _memoizedWidths = {};
-			return function(n){
-			let memoizedWidth = Helpers._memoizedWidths[n];
-			if(memoizedWidth){
-				return memoizedWidth;
-			}
-			 for (let k=1;k<=n;k++){
-			    for(let i =1; i<=n; i++){
-			      if((k*k)>=n){
-			      	memoizedWidth = Helpers._memoizedWidths[n] = 100/k;
-			        return memoizedWidth;
-			      } 
-			    }
-			  }
-			};
-	};
-		
+    findStrings: function(content, strings) {
+        var len = strings.length;
+        var spaceGroups = `\\s*\\r*\\n*\\t*`;
+        var capturingGroup = `\"(.+)\"`
+        
+        var patternString = strings.reduce((res, str, i)=>{
+        res+=`${str}${spaceGroups}:\\s*\\"(.+)\\"`;
+        console.log(i);
+        if (i!==len-1){
+        res+=`\\,${spaceGroups}`;
+        }
+        return res;
+        }, '');
+        //var pattern = "/name\s*\r*\n*\t*:\s*\"(.+)\"\,\s*\r*\n*\t*birthday\s*\r*\n*\t*:\s*\"(.+)\"/g";
+        return patternString;
+    },
+    dayMap: {
+        0: 'sun',
+        1: 'mon',
+        2: 'tue',
+        3: 'wed',
+        4: 'thu',
+        5: 'fri',
+        6: 'sat'
+    },
+    findWidth: function() {
+        var _memoizedWidths = {};
+        return function(n) {
+            let memoizedWidth = Helpers._memoizedWidths[n];
+            if (memoizedWidth) {
+                return memoizedWidth;
+            }
+            for (let k = 1; k <= n; k++) {
+                for (let i = 1; i <= n; i++) {
+                    if ((k * k) >= n) {
+                        memoizedWidth = Helpers._memoizedWidths[n] = 100 / k;
+                        return memoizedWidth;
+                    }
+                }
+            }
+        };
+    }
+
 };
-
-
